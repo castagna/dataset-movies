2b5edb034fb70f0055a04a935e9a38df00794fc9
Paolo Castagna <paolo.castagna@gmail.com>
2012-02-24 14:33:34 +0000

    ...

--- src/main/java/com/kasabi/data/movies/MoviesCommon.java
+++ src/main/java/com/kasabi/data/movies/MoviesCommon.java
@@ -18,11 +18,23 @@
 
 package com.kasabi.data.movies;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.openjena.atlas.io.IO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.sparql.vocabulary.FOAF;
 import com.hp.hpl.jena.tdb.TDBFactory;
 import com.hp.hpl.jena.tdb.base.file.Location;
+import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.util.LocatorURL;
+import com.hp.hpl.jena.util.TypedStream;
 import com.hp.hpl.jena.vocabulary.DC;
 import com.hp.hpl.jena.vocabulary.DCTerms;
 import com.hp.hpl.jena.vocabulary.OWL;
@@ -31,9 +43,13 @@ import com.hp.hpl.jena.vocabulary.RDFS;
 
 public class MoviesCommon {
 
-	public static String KASABI_MOVIES_BASE = "http://data.kasabi.com/dataset/movies/";
-	public static String KASABI_MOVIES_NS = KASABI_MOVIES_BASE ;
-	public static String KASABI_ACTORS_NS = KASABI_MOVIES_BASE + "actors/" ;
+	public static final String DBPEDIA_NS = "http://dbpedia.org/resource/";
+
+	public static final String RDF_FREEBASE_NS = "http://rdf.freebase.com/rdf";
+
+	public static final String KASABI_MOVIES_BASE = "http://data.kasabi.com/dataset/movies/";
+	public static final String KASABI_MOVIES_NS = KASABI_MOVIES_BASE ;
+	public static final String KASABI_ACTORS_NS = KASABI_MOVIES_BASE + "actors/" ;
 	
 	public static Model createModel() {
 		Model model = ModelFactory.createDefaultModel();
@@ -57,5 +73,64 @@ public class MoviesCommon {
 		model.setNsPrefix("freebase", "http://rdf.freebase.com/ns/");
 		model.setNsPrefix("wikipedia", "http://en.wikipedia.org/wiki/");
 	}
+	
+	public static FileManager getFileManager(String namespace) {
+		FileManager fm = FileManager.get();
+		fm.setModelCaching(false);
+		fm.remove(new LocatorURL());
+		fm.addLocator(new LocatorCacheURL(namespace));
+		return fm;
+	}
+}
+
+class LocatorCacheURL extends LocatorURL {
+	
+	private static final Logger log = LoggerFactory.getLogger(LocatorCacheURL.class);
+	
+	private Cache cache;
+	
+	public LocatorCacheURL ( String base ) {
+		super();
+		this.cache = new Cache(base, new File("data/cache"));
+		log.debug("LocatorCacheURL({})", base);
+	}
+
+    @Override
+    public TypedStream open(String url) {
+		log.debug("open({})", url);
+    	if ( cache.has(url) ) {
+    		return new TypedStream(cache.open(url));
+    	} else {
+    		TypedStream ts = super.open(url);
+    		String charset = ts.getCharset();
+    		String mime = ts.getMimeType();
+    		InputStream in = null;
+    		try {
+				String content = IO.readWholeFileAsUTF8(ts.getInput());
+				cache.put(url, content);
+				return new TypedStream( new ByteArrayInputStream(content.getBytes()), mime, charset);
+			} catch (IOException e) {
+				log.error(e.getMessage());
+	    		return null;
+			} finally {
+				if ( in != null ) try { in.close(); } catch (IOException e) {}
+			}
+    	}
+    }
+	
+    @Override
+    public boolean equals( Object other ) {
+        return other instanceof LocatorCacheURL;
+    }
+
+    @Override
+    public int hashCode() {
+        return LocatorCacheURL.class.hashCode();
+    }
+    
+    @Override
+    public String getName() { 
+    	return "LocatorCacheURL"; 
+    } 
 	
 }
